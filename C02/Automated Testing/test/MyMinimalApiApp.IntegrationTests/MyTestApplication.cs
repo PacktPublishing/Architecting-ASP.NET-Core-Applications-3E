c0367c23007b176a95f870fd1afef79659d1ea0b
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-05-26 17:04:44 -0400

    Add MyTestApplicationTest

---
+++ C02/Automated Testing/test/MyMinimalApiApp.IntegrationTests/MyTestApplication.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
+
+namespace MyMinimalApiApp;
+public class MyTestApplication : WebApplicationFactory<Program>
+{
+    
+}
+
+public class MyTestApplicationTest
+{
+    public class Get : ProgramTestWithoutFixture
+    {
+        [Fact]
+        public async Task Should_respond_a_status_200_OK()
+        {
+            // Arrange
+            await using var app = new MyTestApplication();
+            var httpClient = app.CreateClient();
+
+            // Act
+            var result = await httpClient.GetAsync("/");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task Should_respond_hello_world()
+        {
+            // Arrange
+            await using var app = new MyTestApplication();
+            var httpClient = app.CreateClient();
+
+            // Act
+            var result = await httpClient.GetAsync("/");
+
+            // Assert
+            var contentText = await result.Content.ReadAsStringAsync();
+            Assert.Equal("Hello World!", contentText);
+        }
+    }
+}
