696c6f1eae47d4ea9ae39ba36bb7d050ed8300df
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-06-11 16:22:55 -0400

    Add the empty CustomersController

---
+++ C06/MVC.API/Controllers/EmptyCustomersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Data;
+using Shared.Models;
+
+namespace MVC.API.Controllers.Empty;
+
+[Route("empty/[controller]")]
+[ApiController]
+public class CustomersController : ControllerBase
+{
+    // GET: empty/customers
+    [HttpGet]
+    public Task<IEnumerable<Customer>> GetAllAsync(ICustomerRepository customerRepository)
+        => throw new NotImplementedException();
+
+    // GET empty/customers/5
+    [HttpGet("{id}")]
+    public Task<ActionResult<Customer>> GetOneAsync(int id, ICustomerRepository customerRepository)
+        => throw new NotImplementedException();
+
+    // POST empty/customers
+    [HttpPost]
+    public Task<ActionResult> PostAsync([FromBody] Customer value, ICustomerRepository customerRepository)
+        => throw new NotImplementedException();
+
+    // PUT empty/customers/5
+    [HttpPut("{id}")]
+    public Task<ActionResult<Customer>> PutAsync(int id, [FromBody] Customer value, ICustomerRepository customerRepository)
+        => throw new NotImplementedException();
+
+    // DELETE empty/customers/5
+    [HttpDelete("{id}")]
+    public Task<ActionResult<Customer>> DeleteAsync(int id, ICustomerRepository customerRepository)
+        => throw new NotImplementedException();
+}
