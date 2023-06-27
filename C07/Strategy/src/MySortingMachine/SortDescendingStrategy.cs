63badb4b4122584d24380a20bda9835da4925fd0
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-06-26 22:49:43 -0400

    Move old C06 to new C07
    Move the old code, refactor to use file-scoped namespaces, and remove
    unused using statements.

---
+++ C07/Strategy/src/MySortingMachine/SortDescendingStrategy.cs
@@ -0,0 +1,15 @@
+﻿namespace MySortingMachine;
+
+public class SortDescendingStrategy : ISortStrategy
+{
+    public IOrderedEnumerable<string> Sort(IEnumerable<string> input)
+        => input.OrderByDescending(x => x);
+}
+
+public class SortDescendingStrategyClassic : ISortStrategy
+{
+    public IOrderedEnumerable<string> Sort(IEnumerable<string> input)
+    {
+        return input.OrderByDescending(x => x);
+    }
+}
---
+++ C07/Strategy/src/MySortingMachine/SortableCollection.cs
@@ -0,0 +1,21 @@
+﻿namespace MySortingMachine;
+
+public sealed class SortableCollection
+{
+    public ISortStrategy? SortStrategy { get; set; }
+    public IEnumerable<string> Items { get; private set; }
+
+    public SortableCollection(IEnumerable<string> items)
+    {
+        Items = items;
+    }
+
+    public void Sort()
+    {
+        if (SortStrategy == null)
+        {
+            throw new NullReferenceException("Sort strategy not found.");
+        }
+        Items = SortStrategy.Sort(Items);
+    }
+}
