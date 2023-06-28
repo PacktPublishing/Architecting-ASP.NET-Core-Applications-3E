a42d35568a22133c7927b260f53cb09ca7cbd9be
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-06-28 00:08:57 -0400

    Simplify tests

--- C07/AbstractFactory/test/Vehicles.Tests/AbstractFactoryTest.cs
+++ C07/AbstractFactory/test/Vehicles.Tests/AbstractFactoryTest.cs
@@ -3,34 +3,20 @@ using Xunit;
 
 namespace Vehicles;
 
-// Arrange
-public class AbstractFactoryTestCars : AbstractFactoryBaseTestData
-{
-    public AbstractFactoryTestCars()
-    {
-        AddTestData<LowGradeVehicleFactory, LowGradeCar>();
-        AddTestData<HighGradeVehicleFactory, HighGradeCar>();
-        AddTestData<MiddleEndVehicleFactory, MiddleGradeCar>();
-    }
-}
+public class LowGradeVehicleFactoryTest : BaseAbstractFactoryTest<LowGradeVehicleFactory, LowGradeCar, LowGradeBike> { }
+public class MiddleEndVehicleFactoryTest : BaseAbstractFactoryTest<MiddleEndVehicleFactory, MiddleGradeCar, MiddleGradeBike> { }
+public class HighGradeVehicleFactoryTest : BaseAbstractFactoryTest<HighGradeVehicleFactory, HighGradeCar, HighGradeBike> { }
 
-public class AbstractFactoryTestBikes : AbstractFactoryBaseTestData
+public abstract class BaseAbstractFactoryTest<TConcreteFactory, TExpectedCar, TExpectedBike>
+    where TConcreteFactory : IVehicleFactory, new()
 {
-    public AbstractFactoryTestBikes()
+    [Fact]
+    public void Should_create_a_Car_of_the_specified_type()
     {
-        AddTestData<LowGradeVehicleFactory, LowGradeBike>();
-        AddTestData<HighGradeVehicleFactory, HighGradeBike>();
-        AddTestData<MiddleEndVehicleFactory, MiddleGradeBike>();
-    }
-}
+        // Arrange
+        var vehicleFactory = new TConcreteFactory();
+        var expectedCarType = typeof(TExpectedCar);
 
-// Tests
-public class AbstractFactoryTest
-{
-    [Theory]
-    [ClassData(typeof(AbstractFactoryTestCars))]
-    public void Should_create_a_Car_of_the_specified_type(IVehicleFactory vehicleFactory, Type expectedCarType)
-    {
         // Act
         ICar result = vehicleFactory.CreateCar();
 
@@ -38,10 +24,13 @@ public class AbstractFactoryTest
         Assert.IsType(expectedCarType, result);
     }
 
-    [Theory]
-    [ClassData(typeof(AbstractFactoryTestBikes))]
-    public void Should_create_a_Bike_of_the_specified_type(IVehicleFactory vehicleFactory, Type expectedBikeType)
+    [Fact]
+    public void Should_create_a_Bike_of_the_specified_type()
     {
+        // Arrange
+        var vehicleFactory = new TConcreteFactory();
+        var expectedBikeType = typeof(TExpectedBike);
+
         // Act
         IBike result = vehicleFactory.CreateBike();
 
---
+++ C07/AbstractFactory/test/Vehicles.Tests/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0007:Use implicit type", Justification = "<Pending>", Scope = "member", Target = "~M:Vehicles.BaseAbstractFactoryTest`3.Should_create_a_Bike_of_the_specified_type")]
+[assembly: SuppressMessage("Style", "IDE0007:Use implicit type", Justification = "<Pending>", Scope = "member", Target = "~M:Vehicles.BaseAbstractFactoryTest`3.Should_create_a_Car_of_the_specified_type")]
