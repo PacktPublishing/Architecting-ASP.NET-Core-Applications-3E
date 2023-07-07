f131b081271609e23c1ff03d3952e1548f595926
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-07-07 00:53:33 -0400

    Delete MVC code

--- C08/Strategy/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace Strategy.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- C08/Strategy/Program.cs
+++ C08/Strategy/Program.cs
@@ -1,34 +1,3 @@
-﻿using Strategy.Services;
-
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IHomeService, HomeService>();
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-    options.CheckConsentNeeded = context => true;
-    options.MinimumSameSitePolicy = SameSiteMode.None;
-});
-builder.Services.AddControllersWithViews();
-
+﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseCookiePolicy();
-app.UseRouting();
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
-);
-
 app.Run();
