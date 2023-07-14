1f6540235d3876899458b67b9fa7af184cca422a
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-07-14 00:04:27 -0400

    Add Named options samples

--- C09/CommonScenarios/Program.cs
+++ C09/CommonScenarios/Program.cs
@@ -1,41 +1,72 @@
 using CommonScenarios;
 using CommonScenarios.Reload;
 using Microsoft.Extensions.Options;
+using System.Threading;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Manual configuration
 builder.Services.Configure<MyOptions>(myOptions =>
 {
     myOptions.Name = "Default Option";
 });
+
+// Using the settings file
 var defaultOptionsSection = builder.Configuration
     .GetSection("defaultOptions");
 builder.Services
     .Configure<MyOptions>(defaultOptionsSection);
 
+// Named options
+builder.Services.Configure<MyOptions>(
+    "Options1",
+    builder.Configuration.GetSection("options1")
+);
+builder.Services.Configure<MyOptions>(
+    "Options2",
+    builder.Configuration.GetSection("options2")
+);
 
+// Reload
 builder.AddNotificationService();
-builder.Services.Configure<MyOptions>("Options1", builder.Configuration.GetSection("options1"));
-builder.Services.Configure<MyOptions>("Options2", builder.Configuration.GetSection("options2"));
-builder.Services.Configure<MyDoubleNameOptions>(builder.Configuration.GetSection("myDoubleNameOptions"));
-builder.Services.AddTransient<MyNameServiceUsingDoubleNameOptions>();
-builder.Services.AddTransient<MyNameServiceUsingNamedOptionsFactory>();
-builder.Services.AddTransient<MyNameServiceUsingNamedOptionsMonitor>();
-builder.Services.AddTransient<MyNameServiceUsingNamedOptionsSnapshot>();
 
 var app = builder.Build();
-app.MapNotificationService();
+
+// Injecting options
 app.MapGet(
     "/my-options/",
     (IOptions<MyOptions> options) => options.Value
 );
-app.MapGet("/options/{firstOption}", (bool firstOption, MyNameServiceUsingDoubleNameOptions service)
-    => new { name = service.GetName(firstOption) });
-app.MapGet("/factory/{firstOption}", (bool firstOption, MyNameServiceUsingNamedOptionsFactory service)
-    => new { name = service.GetName(firstOption) });
-app.MapGet("/monitor/{firstOption}", (bool firstOption, MyNameServiceUsingNamedOptionsMonitor service)
-    => new { name = service.GetName(firstOption) });
-app.MapGet("/snapshot/{firstOption}", (bool firstOption, MyNameServiceUsingNamedOptionsSnapshot service)
-    => new { name = service.GetName(firstOption) });
+
+// Named options
+app.MapGet(
+    "/factory/{name}",
+    (string name, IOptionsFactory<MyOptions> factory)
+        => factory.Create(name)
+);
+app.MapGet(
+    "/monitor",
+    (IOptionsMonitor<MyOptions> monitor)
+        => monitor.CurrentValue
+);
+app.MapGet(
+    "/monitor/{name}",
+    (string name, IOptionsMonitor<MyOptions> monitor)
+        => monitor.Get(name)
+);
+app.MapGet(
+    "/snapshot",
+    (IOptionsSnapshot<MyOptions> snapshot)
+        => snapshot.Value
+);
+app.MapGet(
+    "/snapshot/{name}",
+    (string name, IOptionsSnapshot<MyOptions> snapshot)
+        => snapshot.Get(name)
+);
+
+// Reload
+app.MapNotificationService();
+
 app.Run();
 
