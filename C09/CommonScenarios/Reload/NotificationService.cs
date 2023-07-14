b6c8a15f48f6174608084c6c221bc315e2045ff6
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-07-14 00:51:27 -0400

    Improve the Reload example

--- C09/CommonScenarios/Reload/NotificationService.cs
+++ C09/CommonScenarios/Reload/NotificationService.cs
@@ -6,19 +6,43 @@ public class NotificationService
 {
     private EmailOptions _emailOptions;
     private readonly ILogger _logger;
+    private IDisposable? _onChangeListener;
+    private readonly IOptionsMonitor<EmailOptions> _monitor;
 
     public NotificationService(IOptionsMonitor<EmailOptions> emailOptions, ILogger<NotificationService> logger)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         ArgumentNullException.ThrowIfNull(emailOptions);
-        emailOptions.OnChange(options => _emailOptions = options);
+        _monitor = emailOptions;
         _emailOptions = emailOptions.CurrentValue;
+        StartListeningForChanges();
     }
 
     public Task NotifyAsync(string to)
     {
-        _logger.LogInformation("Notification sent by '{SenderEmailAddress}' to '{to}'.", _emailOptions.SenderEmailAddress, to);
+        _logger.LogInformation("Notification sent by '{SenderEmailAddress}' to '{to}'. ({monitor})", _emailOptions.SenderEmailAddress, to, _monitor.CurrentValue.SenderEmailAddress);
         return Task.CompletedTask;
+    }
+
+    public void StartListeningForChanges()
+    {
+        _onChangeListener = _monitor.OnChange((options) =>
+        {
+            if (_emailOptions?.SenderEmailAddress != options.SenderEmailAddress)
+            {
+                _logger.LogInformation(
+                    "EmailOptions changed from {old} to {new}.",
+                    _emailOptions?.SenderEmailAddress,
+                    options.SenderEmailAddress
+                );
+                _emailOptions = options;
+            }
+        });
+    }
+
+    public void StopListeningForChanges()
+    {
+        _onChangeListener?.Dispose();
     }
 }
 
@@ -41,6 +65,16 @@ public static class NotificationServiceStartupExtensions
         app.MapPost("notify/{email}", async (string email, NotificationService service) =>
         {
             await service.NotifyAsync(email);
+            return Results.Ok();
+        });
+        app.MapPut("notify", (NotificationService service) =>
+        {
+            service.StartListeningForChanges();
+            return Results.Ok();
+        });
+        app.MapDelete("notify", (NotificationService service) =>
+        {
+            service.StopListeningForChanges();
             return Results.Ok();
         });
         return app;
