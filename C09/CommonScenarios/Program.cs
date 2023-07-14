0b8e3e28da3f46a531f170ea907d392e8d80ba76
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-07-13 22:53:08 -0400

    Configure MyOptions and add an endpoint

--- C09/CommonScenarios/Program.cs
+++ C09/CommonScenarios/Program.cs
@@ -1,7 +1,19 @@
 using CommonScenarios;
 using CommonScenarios.Reload;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MyOptions>(myOptions =>
+{
+    myOptions.Name = "Default Option";
+});
+var defaultOptionsSection = builder.Configuration
+    .GetSection("defaultOptions");
+builder.Services
+    .Configure<MyOptions>(defaultOptionsSection);
+
+
 builder.AddNotificationService();
 builder.Services.Configure<MyOptions>("Options1", builder.Configuration.GetSection("options1"));
 builder.Services.Configure<MyOptions>("Options2", builder.Configuration.GetSection("options2"));
@@ -13,23 +25,17 @@ builder.Services.AddTransient<MyNameServiceUsingNamedOptionsSnapshot>();
 
 var app = builder.Build();
 app.MapNotificationService();
-app.MapGet("/", (HttpContext context) => new[] {
-    new { expecting =  "Options 1", uri = $"https://{context.Request.Host}/options/true" },
-    new { expecting =  "Options 2", uri = $"https://{context.Request.Host}/options/false" },
-    new { expecting =  "Options 1", uri = $"https://{context.Request.Host}/factory/true" },
-    new { expecting =  "Options 2", uri = $"https://{context.Request.Host}/factory/false" },
-    new { expecting =  "Options 1", uri = $"https://{context.Request.Host}/monitor/true" },
-    new { expecting =  "Options 2", uri = $"https://{context.Request.Host}/monitor/false" },
-    new { expecting =  "Options 1", uri = $"https://{context.Request.Host}/snapshot/true" },
-    new { expecting =  "Options 2", uri = $"https://{context.Request.Host}/snapshot/false" },
-});
-app.MapGet("/options/{someCondition}", (bool someCondition, MyNameServiceUsingDoubleNameOptions service)
-    => new { name = service.GetName(someCondition) });
-app.MapGet("/factory/{someCondition}", (bool someCondition, MyNameServiceUsingNamedOptionsFactory service)
-    => new { name = service.GetName(someCondition) });
-app.MapGet("/monitor/{someCondition}", (bool someCondition, MyNameServiceUsingNamedOptionsMonitor service)
-    => new { name = service.GetName(someCondition) });
-app.MapGet("/snapshot/{someCondition}", (bool someCondition, MyNameServiceUsingNamedOptionsSnapshot service)
-    => new { name = service.GetName(someCondition) });
+app.MapGet(
+    "/my-options/",
+    (IOptions<MyOptions> options) => options.Value
+);
+app.MapGet("/options/{firstOption}", (bool firstOption, MyNameServiceUsingDoubleNameOptions service)
+    => new { name = service.GetName(firstOption) });
+app.MapGet("/factory/{firstOption}", (bool firstOption, MyNameServiceUsingNamedOptionsFactory service)
+    => new { name = service.GetName(firstOption) });
+app.MapGet("/monitor/{firstOption}", (bool firstOption, MyNameServiceUsingNamedOptionsMonitor service)
+    => new { name = service.GetName(firstOption) });
+app.MapGet("/snapshot/{firstOption}", (bool firstOption, MyNameServiceUsingNamedOptionsSnapshot service)
+    => new { name = service.GetName(firstOption) });
 app.Run();
 
