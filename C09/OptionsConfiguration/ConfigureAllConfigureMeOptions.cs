7fd14bd5511a8eca84dbca113268d2bc7a81d0f1
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-07-15 17:34:17 -0400

    Remove logging

--- C09/OptionsConfiguration/ConfigureAllConfigureMeOptions.cs
+++ C09/OptionsConfiguration/ConfigureAllConfigureMeOptions.cs
@@ -6,21 +6,11 @@ public class ConfigureAllConfigureMeOptions :
     IPostConfigureOptions<ConfigureMeOptions>,
     IConfigureNamedOptions<ConfigureMeOptions>
 {
-    private readonly ILoggerFactory _loggerFactory;
-    public ConfigureAllConfigureMeOptions(ILoggerFactory loggerFactory)
-    {
-        _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
-    }
-
     public void Configure(ConfigureMeOptions options) // should never be called
         => Configure(Options.DefaultName, options);
 
     public void Configure(string? name, ConfigureMeOptions options)
     {
-        _loggerFactory
-            .CreateLogger($"ConfigureAll name={name}")
-            .LogInformation("Configure")
-        ;
         options.Lines = options.Lines.Append($"ConfigureAll:Configure name: {name}");
         // You can check for the options name or use a switch here instead of
         // checking for "not the default" options.
@@ -32,10 +22,6 @@ public class ConfigureAllConfigureMeOptions :
 
     public void PostConfigure(string? name, ConfigureMeOptions options)
     {
-        _loggerFactory
-            .CreateLogger($"ConfigureAll name={name}")
-            .LogInformation("PostConfigure")
-        ;
         options.Lines = options.Lines.Append($"ConfigureAll:PostConfigure name: {name}");
     }
 }
--- C09/OptionsConfiguration/ConfigureMoreConfigureMeOptions.cs
+++ C09/OptionsConfiguration/ConfigureMoreConfigureMeOptions.cs
@@ -4,18 +4,8 @@ namespace OptionsConfiguration;
 
 public class ConfigureMoreConfigureMeOptions : IConfigureOptions<ConfigureMeOptions>
 {
-    private readonly ILoggerFactory _loggerFactory;
-    public ConfigureMoreConfigureMeOptions(ILoggerFactory loggerFactory)
-    {
-        _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
-    }
-
     public void Configure(ConfigureMeOptions options)
     {
-        _loggerFactory
-            .CreateLogger("ConfigureMore")
-            .LogInformation("Configure")
-        ;
         options.Lines = options.Lines.Append("ConfigureMore:Configure");
     }
 }
