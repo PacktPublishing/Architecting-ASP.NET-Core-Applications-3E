24b0d516d2f9762d56d05c0eea1122886dbaff29
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-07-15 15:56:39 -0400

    Update the configuration sample

--- C09/OptionsConfiguration/Program.cs
+++ C09/OptionsConfiguration/Program.cs
@@ -1,43 +1,99 @@
 using OptionsConfiguration;
 using Microsoft.Extensions.Options;
 
+const string NamedInstance = "MyNamedInstance";
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.PostConfigure<ConfigureMeOptions>(
+    NamedInstance,
+    x => x.Lines = x.Lines.Append("Inline PostConfigure Before")
+);
 builder.Services
     .Configure<ConfigureMeOptions>(builder.Configuration.GetSection("configureMe"))
-    .AddSingleton<IConfigureOptions<ConfigureMeOptions>, Configure1ConfigureMeOptions>()
-    .AddSingleton<IConfigureOptions<ConfigureMeOptions>, Configure2ConfigureMeOptions>()
+    .Configure<ConfigureMeOptions>(NamedInstance, builder.Configuration.GetSection("configureMe"))
+    .AddSingleton<IConfigureOptions<ConfigureMeOptions>, ConfigureAllConfigureMeOptions>()
+    //.AddSingleton<IConfigureNamedOptions<ConfigureMeOptions>, ConfigureAllConfigureMeOptions>()
+    .AddSingleton<IPostConfigureOptions<ConfigureMeOptions>, ConfigureAllConfigureMeOptions>()
+    .AddSingleton<IConfigureOptions<ConfigureMeOptions>, ConfigureMoreConfigureMeOptions>()
 ;
-builder.Services.Configure<ConfigureMeOptions>(options
-    => options.Lines = options.Lines.Append("Another Configure call"));
-builder.Services.PostConfigure<ConfigureMeOptions>(options
-    => options.Lines = options.Lines.Append("What about PostConfigure?"));
-builder.Services.PostConfigureAll<ConfigureMeOptions>(options
-    => options.Lines = options.Lines.Append("Did you forgot about PostConfigureAll?"));
-builder.Services.ConfigureAll<ConfigureMeOptions>(options
-    => options.Lines = options.Lines.Append("Or ConfigureAll?"));
+builder.Services.PostConfigure<ConfigureMeOptions>(
+    NamedInstance,
+    x => x.Lines = x.Lines.Append("Inline PostConfigure After")
+);
+
 builder.Services.AddOptions<ConfigureMeOptions>().Validate(options =>
 {
-    options.Lines = options.Lines.Append("Validate was not intended for this, but this is a trace isn't it?");
+    // Validate was not intended for this, but it works nonetheless...
+    options.Lines = options.Lines.Append("Inline Validate");
     return true;
 });
 
+
 var app = builder.Build();
-app.MapGet("/", (HttpContext context) => new[] { $"https://{context.Request.Host}/configure-me" });
-app.MapGet("/configure-me", (IOptionsMonitor<ConfigureMeOptions> options) => options);
+app.MapGet("/", () => Results.Redirect("/configure-me"));
+app.MapGet(
+    "/configure-me",
+    (IOptionsMonitor<ConfigureMeOptions> options) => new {
+        DefaultInstance = options.CurrentValue,
+        NamedInstance = options.Get(NamedInstance)
+    }
+);
 app.Run();
 
-public class Configure1ConfigureMeOptions : IConfigureOptions<ConfigureMeOptions>
+public class ConfigureMoreConfigureMeOptions : IConfigureOptions<ConfigureMeOptions>
 {
+    private readonly ILoggerFactory _loggerFactory;
+    public ConfigureMoreConfigureMeOptions(ILoggerFactory loggerFactory)
+    {
+        _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+    }
+
     public void Configure(ConfigureMeOptions options)
     {
-        options.Lines = options.Lines.Append("Added line 1!");
+        _loggerFactory
+            .CreateLogger("ConfigureMore")
+            .LogInformation("Configure")
+        ;
+        options.Lines = options.Lines.Append("ConfigureMore:Configure");
     }
 }
 
-public class Configure2ConfigureMeOptions : IConfigureOptions<ConfigureMeOptions>
+
+public class ConfigureAllConfigureMeOptions :
+    IPostConfigureOptions<ConfigureMeOptions>,
+    IConfigureNamedOptions<ConfigureMeOptions>
 {
+    private readonly ILoggerFactory _loggerFactory;
+    public ConfigureAllConfigureMeOptions(ILoggerFactory loggerFactory)
+    {
+        _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+    }
+
+
     public void Configure(ConfigureMeOptions options)
     {
-        options.Lines = options.Lines.Append("Added line 2!");
+        _loggerFactory
+            .CreateLogger("ConfigureAll")
+            .LogWarning("Configure")
+        ;
+        options.Lines = options.Lines.Append("ConfigureAll:Configure");
+    }
+
+    public void Configure(string? name, ConfigureMeOptions options)
+    {
+        _loggerFactory
+            .CreateLogger($"ConfigureAll name={name}")
+            .LogInformation("Configure")
+        ;
+        options.Lines = options.Lines.Append($"ConfigureAll:Configure name: {name}");
+    }
+
+    public void PostConfigure(string? name, ConfigureMeOptions options)
+    {
+        _loggerFactory
+            .CreateLogger($"ConfigureAll name={name}")
+            .LogInformation("PostConfigure")
+        ;
+        options.Lines = options.Lines.Append($"ConfigureAll:PostConfigure name: {name}");
     }
 }
