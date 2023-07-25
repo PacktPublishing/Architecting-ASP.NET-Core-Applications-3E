3a743f1d411a4736d98f68c7a2aad7128f4942b0
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-07-24 21:17:22 -0400

    Add BinarySearchMachineTest

---
+++ C12/test/TemplateMethod.Tests/BinarySearchMachineTest.cs
@@ -0,0 +1,24 @@
+﻿namespace TemplateMethod;
+
+public class BinarySearchMachineTest
+{
+    public class IndexOf
+    {
+        [Theory]
+        [InlineData(1, 0)]
+        [InlineData(8, 5)]
+        [InlineData(3, 2)]
+        [InlineData(7, null)]
+        public void Should_return_the_expected_result(int value, int? expectedIndex)
+        {
+            // Arrange
+            var sorter = new BinarySearchMachine(1, 2, 3, 4, 5, 8);
+
+            // Act
+            var index = sorter.IndexOf(value);
+
+            // Assert
+            Assert.Equal(expectedIndex, index);
+        }
+    }
+}
