065cad98d8e00b0192c056eb621a5c6202be7c78
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-07-24 21:11:32 -0400

    Add LinearSearchMachineTest

---
+++ C12/test/TemplateMethod.Tests/LinearSearchMachineTest.cs
@@ -0,0 +1,25 @@
+namespace TemplateMethod;
+
+public class LinearSearchMachineTest
+{
+    public class Find
+    {
+        [Theory]
+        [InlineData(1, 0)]
+        [InlineData(2, 4)]
+        [InlineData(3, 2)]
+        [InlineData(7, null)]
+        public void Should_return_the_expected_result(int value, int? expectedIndex)
+        {
+            // Arrange
+            var sorter = new LinearSearchMachine(1, 5, 3, 4, 2);
+
+            // Act
+            var index = sorter.IndexOf(value);
+
+            // Assert
+            Assert.Equal(expectedIndex, index);
+        }
+
+    }
+}
