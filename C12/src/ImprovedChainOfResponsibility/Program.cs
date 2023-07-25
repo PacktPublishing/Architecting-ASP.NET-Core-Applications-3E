18c825fc3007b851da570c98132842f12bc58c95
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-07-24 23:48:32 -0400

    Apply previous project changes

--- C12/src/ImprovedChainOfResponsibility/Program.cs
+++ C12/src/ImprovedChainOfResponsibility/Program.cs
@@ -5,31 +5,26 @@ var builder = WebApplication.CreateBuilder(args);
 // ordered by the most called (or the ones to execute earlier)
 // to the less called handler (or the ones that take more time to execute),
 // followed by the DefaultHandler.
-builder.Services.AddSingleton<IMessageHandler>(new AlarmTriggeredHandler(new AlarmPausedHandler(new AlarmStoppedHandler(new DefaultHandler()))));
+builder.Services.AddSingleton<IMessageHandler>(
+    new AlarmTriggeredHandler(
+        new AlarmPausedHandler(
+            new AlarmStoppedHandler(
+                new DefaultHandler()
+            ))));
 
 var app = builder.Build();
-
-// "Menu" endpoint
-app.MapGet("/", () => new[] {
-    "/handle/AlarmTriggered",
-    "/handle/AlarmPaused",
-    "/handle/AlarmStopped",
-    "/handle/SomeUnhandledMessageName",
-});
-
-// Consumer (client) endpoint
-app.MapGet("/handle/{name}", (string name, string? payload, IMessageHandler messageHandler) =>
-{
-    var message = new Message(name, payload);
-    try
+app.MapPost(
+    "/handle",
+    (Message message, IMessageHandler messageHandler) =>
     {
-        // Send the message into the chain of responsibility
-        messageHandler.Handle(message);
-        return $"Message '{message.Name}' handled successfully.";
-    }
-    catch (NotSupportedException ex)
-    {
-        return ex.Message;
-    }
-});
+        try
+        {
+            messageHandler.Handle(message);
+            return $"Message '{message.Name}' handled successfully.";
+        }
+        catch (NotSupportedException ex)
+        {
+            return ex.Message;
+        }
+    });
 app.Run();
