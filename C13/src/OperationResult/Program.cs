99a09b5de9b8867fbd8fd5703d647e26634fad64
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-07-26 21:15:48 -0400

    Add an http file
    Remove the menu and ensure the browser is not open every time since it
    is now possible to query the API from within VS.

--- C13/src/OperationResult/Program.cs
+++ C13/src/OperationResult/Program.cs
@@ -8,18 +8,6 @@ builder.Services
     .AddSingleton<OperationResult.StaticFactoryMethod.Executor>()
 ;
 var app = builder.Build();
-app.MapGet("/", () =>
-{
-    return new[]
-    {
-        "/simplest-form",
-        "/single-error",
-        "/single-error-with-value",
-        "/multiple-errors-with-value",
-        "/multiple-errors-with-value-and-severity",
-        "/static-factory-methods",
-    };
-});
 app.MapGet("/simplest-form", (OperationResult.SimplestForm.Executor executor) =>
 {
     var result = executor.Operation();
