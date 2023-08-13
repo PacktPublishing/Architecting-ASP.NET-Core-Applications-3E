93874285e715bc5b37a95004aa282188a937189f
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-08-12 21:20:29 -0400

    Create the basket feature

--- C18/REPR/Web/Features/Baskets/Baskets.cs
+++ C18/REPR/Web/Features/Baskets/Baskets.cs
@@ -1,30 +1,53 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ForEvolve.ExceptionMapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Web.Features;
 
 public static partial class Baskets
 {
-    public record class ShoppingCart(int CustomerId)
-    {
-        public List<Item> Items { get; } = new();
-    }
-    public record class Item(int ProductId, int Quantity);
+    public record class BasketItem(int CustomerId, int ProductId, int Quantity);
 
     public class BasketContext : DbContext
     {
         public BasketContext(DbContextOptions<BasketContext> options)
             : base(options) { }
 
-        public DbSet<ShoppingCart> ShoppingCarts => Set<ShoppingCart>();
+        public DbSet<BasketItem> Items => Set<BasketItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder
+                .Entity<BasketItem>()
+                .HasKey(x => new { x.CustomerId, x.ProductId })
+            ;
+        }
+    }
+
+    public class DuplicateBasketItemException : ConflictException
+    {
+        public DuplicateBasketItemException(int productId)
+            : base($"The product '{productId}' is already in your shopping cart.")
+        {
+        }
+    }
+
+    public class BasketItemNotFoundException : NotFoundException
+    {
+        public BasketItemNotFoundException(int productId)
+            : base($"The product '{productId}' is not in your shopping cart.")
+        {
+        }
     }
 
     public static IServiceCollection AddBasketsFeature(this IServiceCollection services)
     {
         return services
-            //
-            // TODO: Register endpoints services
-            //
+            .AddAddItem()
+            .AddFetchItems()
+            .AddRemoveItem()
+            .AddUpdateQuantity()
             .AddDbContext<BasketContext>(options => options
                 .UseInMemoryDatabase("BasketContextMemoryDB")
                 .ConfigureWarnings(builder => builder.Ignore(InMemoryEventId.TransactionIgnoredWarning))
@@ -38,9 +61,12 @@ public static partial class Baskets
             .MapGroup(nameof(Baskets).ToLower())
             .WithTags(nameof(Baskets))
         ;
-        //group
-        //    // TODO: Register endpoints maps
-        //;
+        group
+            .MapFetchItems()
+            .MapAddItem()
+            .MapUpdateQuantity()
+            .MapRemoveItem()
+        ;
         return endpoints;
     }
 
