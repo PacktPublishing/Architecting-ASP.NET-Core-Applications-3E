99a11c09f6e82b3f90ea64f454fb55b8182c2011
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-08-17 21:35:59 -0400

    Use the synchronous Add method

--- C18/REPR/Web.Tests/Features/Products/ProductsTest.cs
+++ C18/REPR/Web.Tests/Features/Products/ProductsTest.cs
@@ -7,12 +7,12 @@ public partial class ProductsTest
     private static async Task SeederDelegateAsync(ProductContext db)
     {
         db.Products.RemoveRange(db.Products.ToArray());
-        await db.Products.AddAsync(new Product(
+        db.Products.Add(new Product(
             Name: "Scotch Bottle",
             UnitPrice: 99,
             Id: 2
         ));
-        await db.Products.AddAsync(new Product(
+        db.Products.Add(new Product(
             Name: "Habanero Pepper",
             UnitPrice: 1,
             Id: 3
