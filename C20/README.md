e16dbc4726931083a968e941bc3b113d894694f8
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-08-31 22:40:36 -0400

    Copy C19 to C20

---
+++ C20/README.md
@@ -0,0 +1,46 @@
+# How to start the project using Docker?
+
+## Configure HTTPS on Windows
+
+> Reference: https://learn.microsoft.com/en-us/aspnet/core/security/docker-compose-https
+
+```powershell
+dotnet dev-certs https -ep "$env:APPDATA\ASP.NET\Https\adpg-net8-chapter-19.pfx" -p devpassword
+dotnet dev-certs https --trust
+```
+
+## Build and start the apps
+
+From the `C19` directory:
+
+```bash
+docker-compose up
+```
+
+To shut it down, hit `ctrl+c`, then run the following to destroy the containers:
+
+```bash
+docker-compose down
+```
+
+# How to start both projects manually?
+
+From the `C19` directory:
+
+```bash
+# In one terminal
+cd REPR.Baskets
+dotnet run
+
+# In a second terminal
+cd REPR.Products
+dotnet run
+
+# In a third terminal
+cd REPR.BFF
+dotnet run
+```
+
+# How to start the project from Visual Studio?
+
+You can use the `Docker Compose` target from Visual Studio and start the solution as usual (using `F5` or `ctrl+F5`, for example).
---
+++ C20/REPR.Products/Features/Products/Products.FetchOne.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Riok.Mapperly.Abstractions;
+
+namespace Web.Features;
+
+public partial class Products
+{
+    public partial class FetchOne
+    {
+        public record class Query(int ProductId);
+        public record class Response(int Id, string Name, decimal UnitPrice);
+
+        [Mapper]
+        public partial class Mapper
+        {
+            public partial Response Map(Product product);
+        }
+
+        public class Handler
+        {
+            private readonly ProductContext _db;
+            private readonly Mapper _mapper;
+
+            public Handler(ProductContext db, Mapper mapper)
+            {
+                _db = db ?? throw new ArgumentNullException(nameof(db));
+                _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            }
+
+            public async Task<Response> HandleAsync(Query query, CancellationToken cancellationToken)
+            {
+                var product = await _db.Products.FirstOrDefaultAsync(
+                    x => x.Id == query.ProductId,
+                    cancellationToken: cancellationToken
+                );
+                if (product is null)
+                {
+                    throw new ProductNotFoundException(query.ProductId);
+                }
+                return _mapper.Map(product);
+            }
+        }
+    }
+
+    public static IServiceCollection AddFetchOne(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<FetchOne.Handler>()
+            .AddSingleton<FetchOne.Mapper>()
+        ;
+    }
+
+    public static IEndpointRouteBuilder MapFetchOne(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet(
+            "/{ProductId}",
+            ([AsParameters] FetchOne.Query query, FetchOne.Handler handler, CancellationToken cancellationToken)
+                => handler.HandleAsync(query, cancellationToken)
+        );
+        return endpoints;
+    }
+}
