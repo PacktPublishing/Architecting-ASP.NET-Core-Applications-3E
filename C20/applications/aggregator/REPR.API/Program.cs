ca9c9af2b609718c896cd9b7b429a93aa4bba72b
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-09-03 16:22:42 -0400

    Seed the catalog on start using the IWebClient interface

--- C20/applications/aggregator/REPR.API/Program.cs
+++ C20/applications/aggregator/REPR.API/Program.cs
@@ -1,8 +1,10 @@
+using ApiClient;
 using MassTransit;
 using REPR.Baskets;
 using REPR.Products;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.AddApiClient();
 builder.AddExceptionMapper();
 builder
     .AddBasketModule()
@@ -24,4 +26,17 @@ app
     .MapBasketModule()
     .MapProductsModule()
 ;
+
+// Convenience endpoint, seeding the catalog
+app.MapGet("/", async (IWebClient client, CancellationToken cancellationToken) =>
+{
+    await client.Catalog.CreateProductAsync(new("Banana", 0.30m), cancellationToken);
+    await client.Catalog.CreateProductAsync(new("Apple", 0.79m), cancellationToken);
+    await client.Catalog.CreateProductAsync(new("Habanero Pepper", 0.99m), cancellationToken);
+    return new
+    {
+        Message = "Application started and catalog seeded. Do not refresh this page or will reseed the catalog."
+    };
+});
+
 app.Run();
