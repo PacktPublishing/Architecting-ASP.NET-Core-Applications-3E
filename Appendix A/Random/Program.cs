e4b2348226eea88b89859a8d32f429f2810508a6
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-09-16 15:42:07 -0400

    Add Random

---
+++ Appendix A/Random/Program.cs
@@ -0,0 +1,6 @@
+﻿var items = "Lorem ipsum dolor sit amet".Split(' ');
+var results = Random.Shared.GetItems(items, 1);
+Console.WriteLine(results.Single());
+
+Random.Shared.Shuffle(items);
+Console.WriteLine(string.Join(' ', items));
