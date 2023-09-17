02958628113bdfc2d80ec6ef8f7f0324cf994f94
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-09-16 20:00:39 -0400

    Add RequiredModifier

---
+++ Appendix A/RequiredModifier/Program.cs
@@ -0,0 +1,13 @@
+﻿// Can't do:
+//var entity = new Entity();
+
+// Must do:
+var entity = new Entity()
+{
+    Name = "Test",
+};
+
+public class Entity
+{
+    public required string Name { get; set; }
+}
