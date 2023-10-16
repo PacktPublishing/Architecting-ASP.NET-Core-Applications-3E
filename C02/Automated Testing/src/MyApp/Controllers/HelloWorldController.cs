df84dda73b91ba5b12367e6ce9e73d7458b384b5
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-10-16 14:30:31 -0400

    Make integration test projects better aligned

---
+++ C02/Automated Testing/src/MyApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Controllers;
+
+[Route("")]
+[ApiController]
+public class HelloWorldController : ControllerBase
+{
+    [HttpGet]
+    public string Hello()
+    {
+        return "Hello World!";
+    }
+}
---
+++ C02/Automated Testing/test/MyApp.IntegrationTests/Controllers/HelloWorldControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
+
+namespace MyApp.IntegrationTests.Controllers;
+
+public class HelloWorldControllerTest : IClassFixture<WebApplicationFactory<Startup>>
+{
+    private readonly HttpClient _httpClient;
+    public HelloWorldControllerTest(WebApplicationFactory<Startup> webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+    }
+    public class Hello : HelloWorldControllerTest
+    {
+        public Hello(WebApplicationFactory<Startup> webApplicationFactory)
+            : base(webApplicationFactory) { }
+
+        [Fact]
+        public async Task Should_respond_a_status_200_OK()
+        {
+            // Act
+            var result = await _httpClient.GetAsync("/");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task Should_respond_hello_world()
+        {
+            // Act
+            var result = await _httpClient.GetAsync("/");
+
+            // Assert
+            var contentText = await result.Content.ReadAsStringAsync();
+            Assert.Equal("Hello World!", contentText);
+        }
+    }
+}
