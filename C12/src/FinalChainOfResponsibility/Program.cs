87638e3be97704eba86b584d8d0fed6f78401d97
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-12-17 15:17:21 -0500

    Use Scrutor to register the Chain of responsibility

--- C12/src/FinalChainOfResponsibility/Program.cs
+++ C12/src/FinalChainOfResponsibility/Program.cs
@@ -1,16 +1,28 @@
-﻿using FinalChainOfResponsibility;
+﻿#define USE_SCRUTOR
+using FinalChainOfResponsibility;
 
 var builder = WebApplication.CreateBuilder(args);
 // Create the chain of responsibility,
 // ordered by the most called (or the ones to execute earlier)
 // to the less called handler (or the ones that take more time to execute),
 // followed by the DefaultHandler.
+#if USE_SCRUTOR
+// When using the Decorate method (from Scrutor), we must register the chain
+// in the reverse order.
+builder.Services
+    .AddSingleton<IMessageHandler, DefaultHandler>()
+    .Decorate<IMessageHandler, AlarmStoppedHandler>()
+    .Decorate<IMessageHandler, AlarmPausedHandler>()
+    .Decorate<IMessageHandler, AlarmTriggeredHandler>()
+;
+#else
 builder.Services.AddSingleton<IMessageHandler>(
     new AlarmTriggeredHandler(
         new AlarmPausedHandler(
             new AlarmStoppedHandler(
                 new DefaultHandler()
             ))));
+#endif
 
 var app = builder.Build();
 app.MapPost(
