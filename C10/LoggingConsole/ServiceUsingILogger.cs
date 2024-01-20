387ee045125a7b41e862e76c69ba5e9bb01ffbfe
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-01-20 18:11:58 -0500

    Add LoggingConsole project

---
+++ C10/LoggingConsole/ServiceUsingILogger.cs
@@ -0,0 +1,14 @@
+﻿namespace LoggingConsole;
+
+public class ServiceUsingILogger(ILogger<ServiceUsingILogger> logger)
+{
+    private readonly ILogger _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public string Generate()
+    {
+        _logger.LogInformation("ServiceUsingILogger generating a GUID...");
+        var guid = Guid.NewGuid();
+        _logger.LogDebug("ServiceUsingILogger generated the GUID {guid}.", guid);
+        return guid.ToString();
+    }
+}
