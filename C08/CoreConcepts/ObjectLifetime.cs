4e665b6e53717977f96140983e82f6f62a8473eb
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-02-10 18:28:44 -0500

    Add ObjectLifetime code example

---
+++ C08/CoreConcepts/ObjectLifetime.cs
@@ -0,0 +1,30 @@
+﻿namespace CoreConcepts;
+
+public class ObjectLifetime : ITransient, IScoped, ISingleton
+{
+    public Guid Id { get; } = Guid.NewGuid();
+}
+
+public interface ISingleton
+{
+    Guid Id { get; }
+}
+public interface IScoped
+{
+    Guid Id { get; }
+}
+public interface ITransient
+{
+    Guid Id { get; }
+}
+
+public class ServiceConsumer(ISingleton singleton, IScoped scoped, ITransient transient)
+{
+    private readonly ISingleton _singleton = singleton;
+    private readonly IScoped _scoped = scoped;
+    private readonly ITransient _transient = transient;
+
+    public Guid SingletonId => _singleton.Id;
+    public Guid ScopedId => _scoped.Id;
+    public Guid TransientId => _transient.Id;
+}
--- C08/CoreConcepts/Program.cs
+++ C08/CoreConcepts/Program.cs
@@ -1,6 +1,20 @@
+//
+// Look at the Container.cs and RawClient.cs files for more code
+//
+using CoreConcepts;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<ISingleton, ObjectLifetime>();
+builder.Services.AddScoped<IScoped, ObjectLifetime>();
+builder.Services.AddTransient<ITransient, ObjectLifetime>();
+builder.Services.AddTransient<ServiceConsumer>();
+
 var app = builder.Build();
-
-// Look at the Container.cs and RawClient.cs files
-
+app.MapGet("/", (ServiceConsumer serviceConsumer1, ServiceConsumer serviceConsumer2) =>
+{
+    return TypedResults.Ok(new[] {
+        serviceConsumer1,
+        serviceConsumer2
+    });
+});
 app.Run();
